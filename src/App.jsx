d484fac197aeb2161729536e76aafb3153209f5d
Hanife Alay <hanifealay@Hanife-MacBook-Air.local>
2026-01-29 16:13:46 +0300

    feat: add light and dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './App.css'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Hero from './components/Hero';
+import FloatingThemeToggle from './components/FloatingThemeToggle'
+import { selectMode } from './redux/appSlice';
+import { getAppTheme } from './theme/muiTheme';
 
 function App() {
+  const mode = useSelector(selectMode);
+  const theme = useMemo(() => getAppTheme (mode), [mode]);
  
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <FloatingThemeToggle />
+      <Hero />
+    </ThemeProvider>
   )
 }
 
